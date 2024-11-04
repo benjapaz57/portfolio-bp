f18aaa275874d696cddf9cdcd85506df3332d42f
benja <benjapaz57@gmail.com>
2024-11-04 12:44:19 -0300

    contactttt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { ProjectsComponent } from './components/projects/projects.component';
 
 
 export const routes: Routes = [
+   //Inicio Component
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
     {
         path: "home",
         component: HomeComponent
