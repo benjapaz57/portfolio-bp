21ee0dcedb76549b3af165edd504fbefaad12c23
benja <benjapaz57@gmail.com>
2024-11-05 12:49:12 -0300

    translate 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private translate: TranslateService){}
 }
