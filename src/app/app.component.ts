ce32b012b70917b33c4f18ab2765fc7925300ca6
Benjamin <benjapaz57@gmail.com>
2024-11-06 21:08:30 -0300

    final para cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TranslateModule, TranslateService} from '@ngx-translate/core'
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,12 @@ export class AppComponent {
   translate: TranslateService = inject(TranslateService);
   currentLang: string;
 
-  constructor() {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) 
+  
+  {
     // Cargar el idioma guardado en localStorage o establecer inglés como predeterminado
     const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
     this.translate.use(savedLanguage);
@@ -25,5 +31,12 @@ export class AppComponent {
     this.translate.use(lang);
     localStorage.setItem('selectedLanguage', lang); // Guardar el idioma seleccionado en localStorage
     this.currentLang = lang;
+
+    // Cerrar el menú hamburguesa si está abierto
+    const navbarCollapse = this.document.querySelector('.navbar-collapse');
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+      this.renderer.removeClass(navbarCollapse, 'show');
+    }
   }
 }
+
